a6e15d002d6d07a10b86dcd88762da0341fe0b41
Shiva Shankar <shankaarshiva@gmail.com>
2018-10-22 10:18:39 +0800

    Added MAD and CSAD Lectures ppt and updated projects

---
+++ computer/MAD/LAB/Lab1_main/Lab1a/app/src/main/java/com/sp/restaurantlist/RestaurantList.java
@@ -0,0 +1,13 @@
+package com.sp.restaurantlist;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RestaurantList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
